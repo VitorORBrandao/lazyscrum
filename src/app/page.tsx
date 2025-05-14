83b5088abdabea2ece183d4bd00c62a67cdb9015
Vitor Brandao <vitor.brandao@ufms.br>
2025-05-14 00:26:24 -0400

    style: corrige a estilizacao da tela

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,64 +1,30 @@
 "use client";
 
-import { Bars3Icon } from "@heroicons/react/24/outline";
-import { useState } from "react";
+import { ArrowLongRightIcon } from "@heroicons/react/16/solid";
 
-const navigation = [
-  { name: "Criar Time", href: "#" },
-  { name: "Iniciar Retrospectiva", href: "#" },
-  { name: "Planning Poker", href: "#" },
-];
-
-export default function Example() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
+export default function Home() {
   return (
-    <div className="bg-white">
+    <main className="h-full bg-white">
       <header className="absolute inset-x-0 top-0 z-50">
-        <nav
-          aria-label="Global"
-          className="flex items-center justify-between p-6 lg:px-8"
-        >
-          <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-              <img
-                alt=""
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-              />
-            </a>
-          </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(true)}
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="size-6" />
-            </button>
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12">
-            {navigation.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                className="text-sm/6 font-semibold text-gray-900"
-              >
-                {item.name}
-              </a>
-            ))}
-          </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-primary text-sm/6 font-semibold">
-              Entrar <span aria-hidden="true">&rarr;</span>
-            </a>
-          </div>
+        <nav className="flex items-center justify-between p-6">
+          <a
+            href="#"
+            className="flex items-center gap-1.5 text-sm/6 font-semibold"
+          >
+            <img alt="" src="/logo.png" className="h-8 w-auto" />
+            LazyScrum
+          </a>
+          <a
+            href="#"
+            className="flex items-center gap-1.5 text-sm/6 font-semibold"
+          >
+            Criar time <ArrowLongRightIcon className="size-6" />
+          </a>
         </nav>
       </header>
 
-      <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+      <div className="relative isolate flex h-full items-center justify-center px-6 pt-14 lg:px-8">
+        <div className="max-w-2xl">
           <div className="text-center">
             <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
               Site que ajuda nas cerimonias do scrum
@@ -79,6 +45,6 @@ export default function Example() {
           </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
