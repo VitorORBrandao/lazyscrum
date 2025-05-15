04bc071f0d030db865ce350a2c0524353ba05bf4
Vitor Brandao <vitor.brandao@ufms.br>
2025-05-14 23:22:48 -0400

    style: altera o layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ArrowLongRightIcon } from "@heroicons/react/16/solid";
+import { ArrowLongRightIcon, MoonIcon } from "@heroicons/react/16/solid";
 
 export default function Home() {
   return (
@@ -14,34 +14,29 @@ export default function Home() {
             <img alt="" src="/logo.png" className="h-8 w-auto" />
             LazyScrum
           </a>
-          <a
-            href="#"
-            className="flex items-center gap-1.5 text-sm/6 font-semibold"
-          >
-            Criar time <ArrowLongRightIcon className="size-6" />
-          </a>
+          <div className="rounded-md border-1 border-gray-400 px-1.5 py-1.5">
+            <MoonIcon className="h-4 w-4" />
+          </div>
         </nav>
       </header>
 
       <div className="relative isolate flex h-full items-center justify-center px-6 pt-14 lg:px-8">
-        <div className="max-w-2xl">
-          <div className="text-center">
-            <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-              Site que ajuda nas cerimonias do scrum
-            </h1>
-            <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-              Possui as funcionalidades de retropectiva da sprint e capacidade
-              de realizar um planning poker para ajudar em estimar as
-              atividades.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="bg-secundary hover:bg-primary rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Começar
-              </a>
-            </div>
+        <div className="max-w-2xl text-center">
+          <h1 className="text-5xl font-semibold text-black sm:text-7xl">
+            Simplifique suas cerimônias Scrum
+          </h1>
+          <p className="mt-8 text-lg text-gray-500 sm:text-xl/8">
+            Ferramentas para retrospectivas e planning poker, otimizando a
+            colaboração do seu time e facilitando a estimativa de tarefas.
+          </p>
+          <div className="mt-10 flex items-center justify-center">
+            <a
+              href="#"
+              className="bg-secundary hover:bg-primary flex items-center justify-center gap-2 rounded-md px-3.5 py-2.5 text-sm font-semibold text-white"
+            >
+              Entrar
+              <ArrowLongRightIcon className="h-6 w-6" />
+            </a>
           </div>
         </div>
       </div>
